b1b764ebfabfd6597c88575860484d176b4cafcf
innovacionparticipativa7-netizen <innovacionparticipativa7@gmail.com>
2026-03-02 08:55:03 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+import { auth } from "./firebase-config.js";
+
+import { 
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+// LOGIN
+document.getElementById("loginBtn")?.addEventListener("click", async () => {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const error = document.getElementById("error");
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "dashboard.html";
+  } catch (err) {
+    error.textContent = "Correo o contraseña incorrectos";
+  }
+
+});
+
+// PROTEGER PÁGINAS
+onAuthStateChanged(auth, (user) => {
+  if (!user && window.location.pathname.includes("dashboard.html")) {
+    window.location.href = "index.html";
+  }
+});
